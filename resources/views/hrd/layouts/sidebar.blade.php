10b66aa733dde83200c1a0bd0fa33727860bf7cc
Zhizuuu <zhiddan.a11@gmail.com>
2024-09-21 17:24:49 +0700

    tambahan side-bar

--- resources/views/hrd/layouts/sidebar.blade.php
+++ resources/views/hrd/layouts/sidebar.blade.php
@@ -19,7 +19,7 @@
 
     <!-- Nav Item - Manajemen Karyawan -->
     <li class="nav-item">
-        <a class="nav-link" href=""> 
+        <a class="nav-link" href="{{ route('hrd.manajemen_karyawan') }}"> 
             <i class="fas fa-fw fa-users-cog"></i>
             <span>Manajemen Karyawan</span></a>
     </li>
